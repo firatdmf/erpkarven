072ae4c9135dbee8b74574adab13e5b45cb2a7a4
Muhammed Firat Ozturk <61026405+firatdmf@users.noreply.github.com>
2023-08-18 04:53:52 +0300

    finished django tutorial

--- browndelta/polls/urls.py
+++ browndelta/polls/urls.py
@@ -7,11 +7,13 @@ urlpatterns = [
     # I believe you set the name variables here, and type it in the views folder later on. Also same goes for question_id variable in here
     # names here are used in the template html files (specificially in links to make them easier to manage and read)
     # ex: /polls/
-    path("",views.index,name="index"),
+    path("",views.IndexView.as_view(),name="index"),
     # ex: /polls/5/
-    path("<int:question_id>/",views.detail,name="detail"),
+    # path("<int:question_id>/",views.detail,name="detail"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail" ),
     # ex: /polls/5/results
-    path("<int:question_id>/results/",views.results,name="results"),
+    # path("<int:question_id>/results/",views.results,name="results"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results" ),
     # ex: /polls/5/vote
     path("<int:question_id>/vote/",views.vote,name="vote"),
 
--- browndelta/polls/views.py
+++ browndelta/polls/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse
 # ----
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from .models import Question, Choice
+from .models import Choice
 # ----
 from .models import Question
 # from django.template import loader
@@ -16,47 +16,71 @@ from django.shortcuts import render
 # lets now consider the case of 404 errors
 from django.http import Http404
 from django.shortcuts import get_object_or_404
+from django.views import generic
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    # before it was like below
-    # output = ", ".join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
-    # Then I can also do like below after importing loader from django.template
-    # template = loader.get_template("polls/index.html")
-    # context = {"latest_question_list":latest_question_list,}
-    # return HttpResponse(template.render(context,request))
-    # But even better way to do this which is shown belo
-    context = {"latest_question_list":latest_question_list,}
-    # The render() function takes the request object as its first argument, 
-    # a template name as its second argument and a dictionary as its optional third argument. 
-    # It returns an HttpResponse object of the given template rendered with the given context.
-    return render(request,'polls/index.html',context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     # before it was like below
+#     # output = ", ".join([q.question_text for q in latest_question_list])
+#     # return HttpResponse(output)
+#     # Then I can also do like below after importing loader from django.template
+#     # template = loader.get_template("polls/index.html")
+#     # context = {"latest_question_list":latest_question_list,}
+#     # return HttpResponse(template.render(context,request))
+#     # But even better way to do this which is shown belo
+#     context = {"latest_question_list":latest_question_list,}
+#     # The render() function takes the request object as its first argument, 
+#     # a template name as its second argument and a dictionary as its optional third argument. 
+#     # It returns an HttpResponse object of the given template rendered with the given context.
+#     return render(request,'polls/index.html',context)
 
-def detail(request,question_id):
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404
-    # return render(request,"polls/detail.html",{"question":question})
-    # new method with shortcut
-    # question = get_object_or_404(Question.objects.get(pk=question_id))
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
-def results(request,question_id):
-    # response = "You are looking at the results for question %s."
-    # return HttpResponse(response %question_id)
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,"polls/results.html",{"question":question,})
+# def detail(request,question_id):
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404
+#     # return render(request,"polls/detail.html",{"question":question})
+#     # new method with shortcut
+#     # question = get_object_or_404(Question.objects.get(pk=question_id))
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {"question": question})
+
+# # below is to show after someone votes
+# def results(request,question_id):
+#     # response = "You are looking at the results for question %s."
+#     # return HttpResponse(response %question_id)
+#     question = get_object_or_404(Question,pk=question_id)
+#     return render(request,"polls/results.html",{"question":question,})
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request,question_id):
     # return HttpResponse("You are voting on question %s"%question_id)
+    # let's identify the question or get a 404 error 
     question = get_object_or_404(Question,pk=question_id)
     try:
         # request.POST is a dictionary-like object that lets you access submitted data by key name. In this case, request.POST['choice'] returns the ID of the selected choice, as a string. request.POST values are always strings.
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
         # request.POST['choice'] will raise KeyError if choice wasn’t provided in POST data. The above code checks for KeyError and redisplays the question form with an error message if choice isn’t given.
     except(KeyError,Choice.DoesNotExist):
+        # Redisplay the question voting form.
         return render(
             request,
             "polls/detail.html",
